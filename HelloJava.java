31808e28a5b849d6eae91fb98ed139afc2c390e6
RakheeKaushal <rakhee.kaushal@kritikalvision.ai>
2023-08-11 15:15:45 +0530

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -3,7 +3,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World to the awesome world of Java");
-		  
+		System.out.println("Code for running a loop for generating numbers from 1 to 10 will come here");  
 		  
 	
 
